e33a91fcd6e8fc78e4222a77e0c18b9d63aaf7cf
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2021-01-03 13:10:16 +0900

    Get the current path because the reason is to use json file.

--- AddOnProject/Mysql.cs
+++ AddOnProject/Mysql.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,13 @@ namespace AddOnProject
 {
     public partial class Mysql : Form
     {
-  
-        private static string server = "localhost";
-        private static string db = "saradb";
-        private static string uid = "kdw59520";
-        private static string pwd = "rlaehdnjs12!";
+        private static string current_path = Directory.GetCurrentDirectory() + @"\Mysql";
+        DirectoryInfo cur_path = new DirectoryInfo(current_path);
+
+        private static string server;
+        private static string db;
+        private static string uid;
+        private static string pwd;
         // 밑의 정보를 암호화 해야합니다.
         private static string value = "Server=" + server + ";Database=" + db + ";Uid=" + uid + ";Pwd=" + pwd + ";";
         
@@ -28,10 +31,18 @@ namespace AddOnProject
         public Mysql()
         {
             InitializeComponent();
+            
+            // 폴더 유무 파악하고 없으면 생성하기
+            if (!cur_path.Exists) {
+                MessageBox.Show(current_path + " : " + " File Not Found!!");
+                cur_path.Create(); }
+
+
         }
 
         private void button_Mysql_Insert(object sender, EventArgs e)
         {
+      
             String query = "INSERT INTO user(name, password) VALUES('" + nameBox.Text.ToString() + "', '" + pwdBox.Text.ToString() + "')";
 
             conn.Open();
@@ -41,7 +52,7 @@ namespace AddOnProject
             {
                 if (command.ExecuteNonQuery() == 1)
                 {
-                    MessageBox.Show("INSERT SUCCESS!!");
+                    MessageBox.Show("INSERT SUCCESS!!" + current_path);
                 }
                 else {
                     MessageBox.Show("INSERT FAILED !! ");
