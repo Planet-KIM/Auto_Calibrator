25911c2621f67f6094a010250f7ba5694eab5338
godotw1004 <godotw@naver.com>
2021-01-12 16:19:57 +0900

    add the python sum method in C# program(Test)

---
+++ Test/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var engine = IronPython.Hosting.Python.CreateEngine();
+            var scope = engine.CreateScope();
+
+            try
+            {
+                var source = engine.CreateScriptSourceFromFile(@"Test.py");
+                source.Execute(scope);
+
+                var getPythonFuncResult = scope.GetVariable<Func<string>>("getPythonFunc");
+                Console.WriteLine("def 실행 테스트 : " + getPythonFuncResult());
+
+                var sum = scope.GetVariable<Func<int, int, int>>("sum");
+                //Console.WriteLine(sum(1, 2));
+                int sumvalue =  sum(1, 2);
+                Console.WriteLine(sumvalue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Test/Test/bin/Debug/Test.py
@@ -0,0 +1,9 @@
+
+
+def getPythonFunc():
+    return "Hello"
+
+def sum(a,b):
+    return a+b
+
+print('Hello iron python')
